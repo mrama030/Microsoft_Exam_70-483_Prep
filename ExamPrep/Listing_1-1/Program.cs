0a8a0115893bea859721014d2a5f2d85d6f30480
Mohamed Ali Ramadan <mrama030@uottawa.ca>
2020-02-01 13:23:08 -0500

    first commit

---
+++ ExamPrep/Listing_1-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Listing_1_1
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread secondaryThread = new Thread(new ThreadStart(ThreadMethod));
+            secondaryThread.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main Thread - Processing Line {0}", i);
+            }
+
+            // Wait until secondary thread has completed processing.
+            secondaryThread.Join();
+
+            Console.WriteLine("Main Thread - Complete");
+        }
+
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Secondary Thread - Processing Line {0}", i);
+                // Do not use the entire thread time slice assigned by the OS. 
+                // Give up the remaining time to another thread immediately.
+                Thread.Sleep(0);
+            }
+
+            Console.WriteLine("Secondary Thread - Complete");
+        }
+    }
+}
